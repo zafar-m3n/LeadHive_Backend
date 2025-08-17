78f0f276a7f8479cb6c867cde03f0de3c005333b
zafar-m3n <thezafar.m3n@gmail.com>
2025-08-17 19:24:43 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const morgan = require("morgan");
+const colors = require("colors");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const { connectDB } = require("./config/database");
+
+// ✅ Load env variables
+dotenv.config();
+
+// ✅ Connect to Database
+connectDB();
+
+// ✅ Create Express App
+const app = express();
+
+// ✅ Middleware
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.use(
+  cors({
+    origin: [process.env.NODE_LEADHIVE_FRONTEND_URL, "http://localhost:5173"],
+    credentials: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  })
+);
+
+// ✅ Serve static uploads folder
+app.use("/uploads", express.static("uploads"));
+
+// ✅ Routes
+const authRoutes = require("./routes/authRoutes");
+
+// ✅ Use Routes
+app.use("/api/v1/auth", authRoutes);
+
+// ✅ Root Route
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "LeadHive API is running..." });
+});
+
+// ✅ Define Port
+const PORT = process.env.NODE_LEADHIVE_PORT || 8080;
+
+// ✅ Start Server
+app.listen(PORT, () => {
+  console.log(`LeadHive server running on port ${PORT} in ${process.env.NODE_LEADHIVE_MODE} mode`.bgCyan.white);
+});
