265535333b8569401b5ad095224314014f0bf6d5
zafar-m3n <thezafar.m3n@gmail.com>
2025-08-17 19:46:47 +0530

    Team Model

---
+++ models/Team.js
@@ -0,0 +1,33 @@
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../config/database");
+
+const Team = sequelize.define(
+  "Team",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING(120),
+      allowNull: false,
+      unique: true,
+    },
+    manager_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "users",
+        key: "id",
+      },
+    },
+  },
+  {
+    tableName: "teams",
+    timestamps: false,
+    underscored: true,
+  }
+);
+
+module.exports = Team;
